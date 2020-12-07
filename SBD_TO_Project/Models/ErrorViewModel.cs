5c6baf7b794480983dea0e527bcd9bbe8054dc95
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2020-12-07 12:14:02 +0100

    Add project files.

---
+++ SBD_TO_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SBD_TO_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
