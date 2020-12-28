21aafd6292b2830c4be9f3c31818d7c600435786
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2020-12-28 16:30:50 +0100

    Cinema shared view added

---
+++ SBD_TO_Project/Controllers/CinemaBrowseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SBD_TO_Project.Data;
+using SBD_TO_Project.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBD_TO_Project.Controllers
+{
+    public class CinemaBrowseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CinemaBrowseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            CinemaBrowseVM cinemaBrowseVM = new CinemaBrowseVM()
+            {
+                Cinemas = _db.Cinema,
+                Cities = _db.Cinema.Select(c => c.Town).Distinct()
+            };
+            return View(cinemaBrowseVM);
+        }
+    }
+}
