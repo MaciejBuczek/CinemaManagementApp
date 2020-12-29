b61afca2ebcd35684c7e5e6246b1e925cfdde857
ga.kamil98@gmail.com <ga.kamil98@gmail.com>
2020-12-29 14:14:34 +0100

    Added directory functionality (plus some fixes with Screening room)

---
+++ SBD_TO_Project/Controllers/DirectorController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SBD_TO_Project.Data;
+using SBD_TO_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBD_TO_Project.Controllers
+{
+    public class DirectorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DirectorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Director> objList = _db.Director.Include(m => m.Movies);
+            return View(objList);
+        }
+
+        //Create Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Create Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Director obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Edit Get
+        public IActionResult Edit(int id)
+        {
+            var obj = _db.Director.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        //TestCommit
+        //Edit Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Director obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Delete Get
+        public IActionResult Delete(int id)
+        {
+            var obj = _db.Director.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //Delete Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var obj = _db.Director.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
